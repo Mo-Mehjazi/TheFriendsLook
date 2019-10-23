617dfba1ff5cc6b94b8b3ed42191119057e762fd
Jan Lafferton <jan.lafferton@gmx.de>
2019-10-23 18:01:42 +0200

    Event page improvements

--- lib/event_card.dart
+++ lib/event_card.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+const eventCardFontColor = Colors.black;
+const eventCardWhiteColor = Colors.white38;
+
 class Event {
   String title;
   String author;
   String description;
   int numSubscribers;
   int maxSubscribers;
-  String location;
+  String imageUrl;
 
   Event(this.title, this.author, this.description, this.numSubscribers,
-      this.maxSubscribers, this.location);
+      this.maxSubscribers, this.imageUrl);
 }
 
 class EventCard extends StatelessWidget {
@@ -22,86 +25,88 @@ class EventCard extends StatelessWidget {
     return Card(
       semanticContainer: true,
       clipBehavior: Clip.antiAliasWithSaveLayer,
-      child: Stack(
-        alignment: Alignment.topLeft,
-        children: <Widget>[
-          Image.network(
-            'https://i.pinimg.com/originals/6d/78/96/6d7896263c68fca18d1ee10e44dd4160.jpg',
-            fit: BoxFit.fill,
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      event.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
-                    ),
-                    Chip(
-                      label: Text(event.author),
-                      backgroundColor: Colors.white30,
-                    ),
-                    Chip(
-                      backgroundColor: Colors.white30,
-                      label: Text(
-                        '${event.numSubscribers} / ${event.maxSubscribers}',
-                        style: TextStyle(
-                            //color: Colors.white,
-                            //fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                    ),
-                  ],
-                ),
-                Text(
-                  event.description,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
+      child: Container(
+        child: Stack(
+          alignment: Alignment.topLeft,
+          children: <Widget>[
+            Image.network(
+              event.imageUrl,
+              fit: BoxFit.fill,
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.white,
-                      ),
                       Text(
-                        event.location,
+                        event.title,
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          color: Colors.white,
+                          fontSize: 18,
+                          color: eventCardFontColor,
+                        ),
+                      ),
+                      Chip(
+                        label: Text(event.author),
+                        backgroundColor: eventCardWhiteColor,
+                      ),
+                      Chip(
+                        backgroundColor: eventCardWhiteColor,
+                        label: Text(
+                          '${event.numSubscribers} / ${event.maxSubscribers}',
+                          style: TextStyle(
+                              //color: Colors.white,
+                              //fontWeight: FontWeight.bold,
+                              ),
                         ),
                       ),
                     ],
                   ),
-                  padding: EdgeInsets.all(6),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.white30,
+                  Text(
+                    event.description,
+                    style: TextStyle(
+                      color: eventCardFontColor,
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(
+                          Icons.location_on,
+                          color: eventCardFontColor,
+                        ),
+                        Text(
+                          'View in Maps',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: eventCardFontColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    padding: EdgeInsets.all(6),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: eventCardWhiteColor,
+                    ),
+                  ),
+                ],
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: eventCardWhiteColor,
+              ),
             ),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white30,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
