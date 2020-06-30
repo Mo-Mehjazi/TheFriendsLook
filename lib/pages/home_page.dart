8f2e19ccbd1874679d534f14fdf0c94b9c1f1705
Jan Lafferton <jan@lafferton.de>
2020-06-30 10:38:15 +0200

    Background image

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,32 +6,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      drawer: UserDrawer(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.pushNamed(context, '/create');
-        },
-        child: Icon(Icons.add)
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 10
+        appBar: AppBar(
+          title: Text('Home Page'),
+        ),
+        drawer: UserDrawer(),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/create');
+            },
+            child: Icon(Icons.add)),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/HG1.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: Column(
             children: <Widget>[
-              Image.asset(
-                'assets/images/Logo.JPG',
-                scale: 3.5,
-              ),
+              Container(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/images/Logo.JPG',
+                    scale: 3.5,
+                  ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        ));
   }
 }
